35384d4ce9bb9f90fc98477770467761c2c912c7
“raghav” <bahetiraghav990@gmail.com>
2024-09-20 14:32:51 +0530

    redux

--- frontend/frontend tr-2/src/App.jsx
+++ frontend/frontend tr-2/src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Signup from './pages/Signup'
 import { Routes,Route } from 'react-router-dom'
-import { Toaster } from 'sonner';
+import { Toaster } from 'sonner'; 
+import { useSelector } from 'react-redux';
 const App = () => {
+  const { count } = useSelector((state) => {
+    console.log(state.count);
+    state.count
+  });
+
   return (
     <div>
+      <p>count:{count}</p>
+      <button>Increament</button>
+      <button>Decreament</button>
       <Routes>
         <Route path='/register' element={<Signup />} />
       </Routes>
-      <Toaster position="top-right"/>
+      <Toaster position="top-right" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
