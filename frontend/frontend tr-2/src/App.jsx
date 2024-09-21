9161a3ad7a8d99e43a2b76551582f2b751578fef
“raghav” <bahetiraghav990@gmail.com>
2024-09-21 09:43:49 +0530

    appjsx

--- frontend/frontend tr-2/src/App.jsx
+++ frontend/frontend tr-2/src/App.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import Signup from './pages/Signup'
 import { Routes,Route } from 'react-router-dom'
 import { Toaster } from 'sonner'; 
-import { useSelector } from 'react-redux';
-const App = () => {
-  // const { count } = useSelector((state) => {
-  //   console.log(state.count);
-  //   state.count
-  // });
+import React from 'react'
+import Signup from './pages/Signup'
+import { Routes,Route } from 'react-router-dom'
+import { Toaster } from 'sonner'; 
+import { useSelector, useDispatch } from 'react-redux';
+import { increment,decrement } from './redux/counterSlice';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+  console.log(state.count.count)
+  state.count.count
+}
+const handleIncreament = () => {
+  dispatch(increment());
+}
+const handleDecreament = () => {
+  dispatch(decrement());
+}
 
   return (
     <div>
-      {/* <p>count:{count}</p> */}
-      {/* <button>Increament</button>
-      <button>Decreament</button> */}
+      <p>count:{count}</p>
+      <button onClick={handleIncreament}>Increament</button>
+      <button onClick={() => dispatch(decrement())}>Decreament</button>
       <Routes>
         <Route path='/register' element={<Signup />} />
       </Routes>
@@ -21,5 +34,3 @@ const App = () => {
     </div>
   );
 }
-
-export default App;
