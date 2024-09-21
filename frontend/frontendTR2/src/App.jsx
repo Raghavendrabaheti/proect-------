72cc6bd6d66ea2a98e33f7e3c23b5293e04705d7
“raghav” <bahetiraghav990@gmail.com>
2024-09-21 21:34:22 +0530

    update

--- frontend/frontendTR2/src/App.jsx
+++ frontend/frontendTR2/src/App.jsx
@@ -9,8 +9,6 @@ export default function App() {
   const dispatch = useDispatch();
   const {count} = useSelector((state) => state.count);
   // console.log(state.count.count)
-  
-
 
   const handleIncreament = () => {
     dispatch(increment());
@@ -27,7 +25,22 @@ export default function App() {
       <Routes>
         <Route path='/register' element={<Signup />} />
       </Routes>
-      <Toaster position="top-right" />
+      <Toaster position="bottom-right" />
     </div>
   );
 }
+
+//NOTE keywords in react-redux and @reduxtoolkit
+//actions => actions are the javascript object which have type and payload property
+
+//reducers => takes the previous state and returns new state
+
+//useDispatch
+//createSlice => it generally a function which manages the reducers and actions 
+
+//store => store is like a container which have all the state we are using in our project
+
+//initialState => initial value of the some value which is going to change
+
+//useSelector
+//provider
