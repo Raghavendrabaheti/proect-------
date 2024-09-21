ef564113731481571398e957e8dd3854729ee17e
“raghav” <bahetiraghav990@gmail.com>
2024-09-21 10:00:11 +0530

    updated

--- frontend/frontend tr-2/src/App.jsx
+++ frontend/frontend tr-2/src/App.jsx
@@ -2,25 +2,22 @@ import React from 'react'
 import Signup from './pages/Signup'
 import { Routes,Route } from 'react-router-dom'
 import { Toaster } from 'sonner'; 
-import React from 'react'
-import Signup from './pages/Signup'
-import { Routes,Route } from 'react-router-dom'
-import { Toaster } from 'sonner'; 
 import { useSelector, useDispatch } from 'react-redux';
 import { increment,decrement } from './redux/counterSlice';
 
 export default function App() {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
-  console.log(state.count.count)
-  state.count.count
-}
-const handleIncreament = () => {
-  dispatch(increment());
-}
-const handleDecreament = () => {
-  dispatch(decrement());
-}
+  const {count} = useSelector((state) => state.count);
+  // console.log(state.count.count)
+  
+
+
+  const handleIncreament = () => {
+    dispatch(increment());
+  }
+  const handleDecreament = () => {
+    dispatch(decrement());
+  }
 
   return (
     <div>
