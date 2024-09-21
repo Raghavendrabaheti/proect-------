2d676ae35d7460e458fa3351b9b3dd7e5e3cb444
“raghav” <bahetiraghav990@gmail.com>
2024-09-21 10:34:45 +0530

    userSlice

--- frontend/frontend tr-2/src/redux/userSlice.js
+++ frontend/frontend tr-2/src/redux/userSlice.js
@@ -1,5 +1,38 @@
 // Signup
+import { createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = { 
-    
- }
+ export const Signup = createAsyncThunk('/user/signup', async (data, { rejectWithValue }) => {
+    try {
+        const res = await axios.post('http://localhost:3000/api/register', data);
+        return res.data;
+    }
+    catch (error) {
+        return rejectWithValue(error);
+    }
+});
+const initialState = {  
+    loading: false,
+    error:null
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(Signup.pending, (state) => 
+            {
+                state.loading = true;
+                state.error = null;
+            }).addCase(Signup.fulfilled,(state)=>{
+                state.loading = false;
+                state.error = null;
+            }).addCase(Signup.rejected,(state,action)=>{
+                state.loading = false;
+                state.error = action.payload;
+            })
+    }
+});
+
+export default userSlice.reducer;
